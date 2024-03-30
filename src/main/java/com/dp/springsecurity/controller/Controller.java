dc0f1a0ec8e3fdcb500dc5cff4b4355c155076f1
Dinesh Thota <dineshthota679@gmail.com>
2024-03-30 16:18:24 +0530

    Jwt Token impl modified commit message

--- src/main/java/com/dp/springsecurity/controller/Controller.java
+++ src/main/java/com/dp/springsecurity/controller/Controller.java
@@ -1,10 +1,13 @@
 package com.dp.springsecurity.controller;
 
+import com.dp.springsecurity.entity.AuthRequest;
 import com.dp.springsecurity.entity.UserInfo;
 import com.dp.springsecurity.repository.UserInfoRepo;
+import com.dp.springsecurity.service.JwtToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,14 +23,20 @@ public class Controller {
     @Autowired
     private UserInfoRepo userInfoRepo;
 
+    @Autowired
+    private JwtToken jwtToken;
+
+
     @GetMapping("/products/emp")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<String> getEmployee(){
         return ResponseEntity.status(HttpStatus.OK).body("Employee");
     }
 
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     @GetMapping("/products/get")
     public ResponseEntity<String> getProduct(){
-        return ResponseEntity.status(HttpStatus.OK).body("Employee");
+        return ResponseEntity.status(HttpStatus.OK).body("Product");
     }
 
     @PostMapping("/products/new")
@@ -35,6 +44,12 @@ public class Controller {
         userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
         userInfoRepo.save(userInfo);
         return ResponseEntity.status(HttpStatus.CREATED).body("User Created");
+    }
+
+    @PostMapping("/authenticate")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest){
+
+        return jwtToken.generateToken(authRequest.getUserName());
     }
 
 }
---
+++ src/main/java/com/dp/springsecurity/service/JwtToken.java
@@ -0,0 +1,38 @@
+package com.dp.springsecurity.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtToken {
+    String SECRET = "a67c9281bc71988250a90639d81a84b3b5db5ce2f1a68964467ca5d4d9adeccf";
+
+    public String generateToken(String userName){
+
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userName);
+    }
+
+    private String createToken(Map<String, Object> claims, String userName) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userName)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*30))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key getSignKey() {
+
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
