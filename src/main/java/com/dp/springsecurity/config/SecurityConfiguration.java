d59057e9d4b34964b2d10c8b0b19e10f0f60ceb5
Dinesh Thota <dineshthota679@gmail.com>
2024-03-30 16:18:24 +0530

    Jwt Token impl

--- src/main/java/com/dp/springsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/dp/springsecurity/config/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.dp.springsecurity.config;
 
+import com.mysql.cj.protocol.AuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -18,7 +20,7 @@ public class SecurityConfiguration {
 
     @Bean
     //authenticate
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+    public UserDetailsService userDetailsService(){
 //        UserDetails admin = User.withUsername("Dinesh")
 //                .password(passwordEncoder.encode("pwd"))
 //                .roles("ADMIN").build();
@@ -30,18 +32,26 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-        @Bean
+    //authorization
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/products/welcome", "/products/new").permitAll()
+                .requestMatchers("/products/new","/authenticate").permitAll()
                 .and()
                 .authorizeHttpRequests().requestMatchers("/products/**")
                 .authenticated().and().formLogin().and().build();
     }
 
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
 }
